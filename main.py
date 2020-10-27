e17b896cad3a56de27d723c9ffe558304a8ba499
wterkin <appmailbox@mail.ru>
2020-10-27 14:59:25 +0300

    build interface

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
  
-from tkinter import Tk, Frame, BOTH
+from tkinter import Tk, BOTH
+from tkinter.ttk  import Frame, Button, Style
 
 import config
 
 MAIN_WINDOW_TITLE = "Forget-Me-Not version 0,1"
-MAIN_WINDOW_WIDTH = 290
-MAIN_WINDOW_HEIGHT = 150
+MAIN_WINDOW_WIDTH = 640 
+MAIN_WINDOW_HEIGHT = 480
 
 class MainWindow(Frame):
     
     def __init__(self, po_parent):
+        """Конструктор."""
         Frame.__init__(self, po_parent) # background = "white"
         self.parent = po_parent
+        self.construct_window()
+
+    def construct_window(self):
+        """Создает интерфейс окна."""
         self.parent.title(MAIN_WINDOW_TITLE)
+        self.style = Style()
+        self.style.theme_use("default")
         self.pack(fill=BOTH, expand=1)
         self.centerWindow()
-
-
+        lo_quit_button = Button(self, text="Выйти", command=self.quit)
+        lo_quit_button.place(x=50, y=50)
+        
     def centerWindow(self):
         li_window_width = MAIN_WINDOW_WIDTH
         li_window_height = MAIN_WINDOW_HEIGHT
@@ -31,9 +40,9 @@ class MainWindow(Frame):
         
 
 def main():
-    application = Tk()
-    main_window = MainWindow(application)
-    application.mainloop()
+    root = Tk()
+    application = MainWindow(root)
+    root.mainloop()
  
 if __name__ == '__main__':
     main()    
