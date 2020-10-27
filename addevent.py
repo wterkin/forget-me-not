2308f545b267bfaef154b99d565d5ecb5aa4a659
wterkin <appmailbox@mail.ru>
2020-10-27 20:29:50 +0300

    developing in progress

--- addevent.py
+++ addevent.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
 
+class EventEditor:
+    def __init__(self, root):
+        self.event_name = Entry(root, width=20)
+        self.event_label = Label(root, width=20, text="Название события")  #  bg='black', fg='white', )
+        self.ok_button = Button(root, text="Принять")
+        self.cancel_button = Button(root, text="Отмена")
+        self.event_name.pack()
+        self.event_label.pack()
+        self.ok_button.pack()
+        self.cancel_button.pack()
+        
+    #b1['text'] = "Изменено"
+    #b1['bg'] = '#000000'
+    #b1['activebackground'] = '#555555'
+    #b1['fg'] = '#ffffff'
+    #b1['activeforeground'] = '#ffffff'
+    #b1.config(command=change)
+    
 if __name__ == '__main__':
     root = Tk()
-    event_name = Entry(root, width=20)
-    event_label = Label(root, width=20, text="Название события")  #  bg='black', fg='white', )
-    ok_button = Button(root, text="Принять")
-    cancel_button = Button(root, text="Отмена")
-    event_name.pack()
-    event_label.pack()
-    ok_button.pack()
-    cancel_button.pack()
+    event_editor = EventEditor(root)
     root.mainloop()
