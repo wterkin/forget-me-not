714ac50267c8c6458f0ca5752760ef2c1d4df7a7
wterkin <appmailbox@mail.ru>
2020-10-29 17:36:35 +0300

    database model developing-2

--- database.py
+++ database.py
@@ -2,21 +2,24 @@
 ## -*- coding: utf-8 -*-
 
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
+Base = declarative_base()
 
-class EventTypes(object):
-        id = Column(Integer,
-                    autoincrement=True,
+class CEventType(object):
+    __tablename__ = 'tbl_types'
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    fname = Column(String,
                     nullable=False,
-                    primary_key=True,
                     unique=True)
-        fname = Column(String,
-                       nullable=False,
-                       unique=True)
-        fcolor = Column(Integer)
-        fstatus = Column(Integer,
-                         nullable=False)
+    fcolor = Column(Integer)
+    fstatus = Column(Integer,
+                        nullable=False)
     def __init__(self, pname, pcolor):
         
         self.fname = pname
@@ -26,3 +29,18 @@ class EventTypes(object):
     def __repr__(self):
         
         return f"ID:{self.id}, Name:{self.fname}, Color:{self.fcolor}"
+    
+class CEvent(object):
+    __tablename__ = 'tbl_events'    
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    fday = Column(Integer,
+                  nullable=False)
+    fmonth = Column(Integer,
+                    nullable=False)    
+    fyear = Column(Integer,
+                   nullable=False)    
+    #ftype =  
